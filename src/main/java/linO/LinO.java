2f89fd741345a790e63e8c4bd9f0116091e01065
leticiacoelho <leticia.ac23@gmail.com>
2019-11-25 18:54:18 -0300

    API

--- src/main/java/linO/LinO.java
+++ src/main/java/linO/LinO.java
@@ -1,11 +1,11 @@
 package linO;
 
-public class Greeting {
+public class LinO {
 
     private final long id;
     private final String content;
 
-    public Greeting(long id, String content) {
+    public LinO(long id, String content) {
         this.id = id;
         this.content = content;
     }
