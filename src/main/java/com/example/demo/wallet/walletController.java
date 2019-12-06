ddb33a3dc0532aa5345b3a9848760c56bdf3f442
leticiacoelho <leticia.ac23@gmail.com>
2019-12-06 09:45:27 -0300

    Implementa a função insertWallet

--- src/main/java/com/example/demo/wallet/walletController.java
+++ src/main/java/com/example/demo/wallet/walletController.java
@@ -1,31 +1,33 @@
 package com.example.demo.wallet;
 
+import com.example.demo.model.User;
 import com.example.demo.model.Wallet;
 import com.example.demo.repository.WalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+@RequestMapping("/wallet")
 
 @RestController
 public class walletController {
 
     @Autowired
     private WalletRepository walletRepository;
-    /* Informa o saldo atual de um usuário. */
-    @GetMapping("/")
-    public ResponseEntity<String> Hello() {
-        return new ResponseEntity<String>(
-                "Hello!", HttpStatus.OK
-        );
+
+    /* Insere nova carteira. */
+    @PostMapping
+    public Wallet insert(@RequestBody Wallet wallet){
+        return walletRepository.save(wallet);
     }
 
+
+
     /* Informa o saldo atual de um usuário. */
-    @GetMapping("/verifyWallet")
+    @GetMapping
     public ResponseEntity<List<Wallet>> verifyWallet() {
         return new ResponseEntity<List<Wallet>>(
                 walletRepository.findAll(), HttpStatus.OK
@@ -33,7 +35,7 @@ public class walletController {
     }
 
     /* Atualiza informações sobre os gastos de um grupo. */
-    @PostMapping("/updateWallet")
+    @PostMapping("/{id}")
     public ResponseEntity<String> updateWallet() {
         return new ResponseEntity<String>(
                 "updateWallet", HttpStatus.OK
